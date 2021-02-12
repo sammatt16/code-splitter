65eba14d1e661a10df6c8440d58623def2525b29
unknown <samatth16@gmail.com>
2021-02-12 13:59:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import { Component } from 'react';
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1',
+      component: ''
+    }
+  }
+
+  onRouteChange = (route) => {
+    // No Code Splitting
+    // this.setState({route: route});
+    // With Code Splitting
+    if (route === 'page1'){
+      this.setState({route: route})
+    }else if (route === 'page2'){
+      import('./components/Page2').then((Page2) => {
+        this.setState({route: route, component: Page2.default})
+      })
+    }else if (route === 'page3'){
+      import('./components/Page3').then((Page3) => {
+        this.setState({route: route, component: Page3.default})
+      })
+    }
+  } 
+  render(){
+    // if (this.state.route === 'page1'){
+    //   return <Page1 onRouteChange={this.onRouteChange}/>
+    // } else if(this.state.route === 'page2'){
+    //   return <Page2 onRouteChange={this.onRouteChange}/>
+    // }else if(this.state.route === 'page3'){
+    //   return <Page3 onRouteChange={this.onRouteChange}/>
+    // }
+    if (this.state.route === 'page1'){
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />
+    }
+  }
 }
 
 export default App;
